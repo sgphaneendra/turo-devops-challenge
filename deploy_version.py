060c871eb17ead5d8362bada8081d352c57dd6fa
sgphaneendra <159864796+sgphaneendra@users.noreply.github.com>
2024-02-16 03:43:07 +0530

    Update deploy_version.py

--- deploy_version.py
+++ deploy_version.py
@@ -50,7 +50,7 @@ class TagUpdate:
 
             data["image_name"] = "sgphaneendra/nginx-static-app:" + self.new_image_tag
 
-            with open("vars.tfvars.json", "w") as file_out:
+            with open("terraform/terraform.tfvars", "w") as file_out:
                 file_out.write(json.dumps(data, indent=4))
 
         except Exception as e:
