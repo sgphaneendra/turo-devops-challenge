d13c32203c31cae950fcf984d6e424e4237dd2d3
sgphaneendra <159864796+sgphaneendra@users.noreply.github.com>
2024-02-16 04:08:01 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,4 +1,10 @@
 terraform {
-  backend "local" {
+  backend "s3" {
+    bucket = "terraform-tfstate-sai"
+    key    = "terraform/state"
+    region = "us-east-1"
   }
+}
+provider "aws" {
+  region = "us-east-1"
 }
